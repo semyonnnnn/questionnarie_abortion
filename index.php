d521c7dd405859170bd9b2f1d48207129d6b50fb
Семён Максимович Гусев <145036030+Kanoe99@users.noreply.github.com>
2025-08-06 11:10:47 +0500

    dockerize: docker

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-
-echo "hello bot";
+echo '<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>My App</title>
+</head>
+<body>
+  <h1>Hello from PHP</h1>
+</body>
+</html>';
+?>
